84fd7098a5bf59e26e5be6672581f6705f80f6fc
DPI-PTTL-034 <nrufi@uic.edu>
2026-06-09 13:36:53 -0500

    adding 8 coded parts

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main(){
+
+	fmt.Println("I am"+" hungry")
+	fmt.Println("1+1= ", 2+0)
+	fmt.Println("1+1= ", 7.0/3.0)
+
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
